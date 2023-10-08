8b64f68f224d934e5652968a6b0047744605c517
noahwillson <nwillson2601@gmail.com>
2023-10-08 20:36:46 +0300

    meta tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import Logo from "../images/Logo.svg"
 import Nav from "./Nav"
+import MetaTags from "react-meta-tags";
 const Header = () => {
     return (
         <header>
+            <MetaTags>
+            <meta name="description" content="This is a description"/>
+            <meta name="og:title" content="Little Lemon" />
+            <meta name="og:description" content="This is a description" />
+            <meta name="og:image" content="https://www.littlelemon.com/images/logo.svg" />
+            </MetaTags>
             <img src={Logo}/>
             <Nav/>
         </header>
