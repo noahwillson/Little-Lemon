cc0083fed0ca11823626c85bf122e02f0d831ddc
Pegasus <nwillson2601@gmail.com>
2023-10-09 19:05:46 +0300

    tailwind installed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Fragment className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-        <Header className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10"/>
+    <Fragment>
+        <Header/>
         <Main/>
         <Footer/>
 
