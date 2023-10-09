336152a01802d1a55caaca1491d028c0928c7daa
Pegasus <nwillson2601@gmail.com>
2023-10-09 23:14:04 +0300

    hello

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,16 @@ import Nav from "./Nav"
 import React from "react";
 const Header = () => {
     return (
-        <header>
+        <div>
+        <header className="flex justify-content-between align-items-center">
                 <meta name="description" content="This is a description"/>
                 <meta name="og:title" content="Little Lemon" />
                 <meta name="og:description" content="This is a description" />
                 <meta name="og:image" content="https://www.littlelemon.com/images/logo.svg" />
             <img src={Logo} alt="Little Lemon"/>
-            <Nav/>
         </header>
+        <Nav/>
+        </div>
     )
 }
 export default Header
