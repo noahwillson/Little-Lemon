63dacc4348adc1e72573667242662ea92ab4d50e
Pegasus <nwillson2601@gmail.com>
2023-10-10 11:50:51 +0300

    main ui

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,32 +4,51 @@ import greeksalad from "../images/greek salad.jpg";
 import bruchetta from "../images/bruchetta.svg";
 import lemondessert from "../images/lemon dessert.jpg";
 const Main = () => {
-    return (
-        <main>
-            <section>
-                <h1>little lemon</h1>
-                <h3>Chicago</h3>
-                <button>Reserv a Table</button>
-                <img src={img} alt="chef holds food" />
-            </section>
-            <section>
-                <h1>This weeks specials</h1>
-                <button>Online Menu</button>
-                <img src={greeksalad} alt="greek salad"/>
-                <p>12.99</p>
-                <p>lorem ipsum dolor sit amet consectetur</p>
-                <button>Order a delivery</button>
-                <img src={bruchetta} alt="bruchetta"/>
-                <p>5.99</p>
-                <p>lorem ipsum dolor sit amet consectetur</p>
-                <button>Order a delivery</button>
-                <img src={lemondessert} alt="lemondessert"/>
-                <p>5.00</p>
-                <p>lorem ipsum dolor sit amet consectetur</p>
-                <button>Order a delivery</button>
-            </section>
-        </main>
-    );
+  return (
+    <main>
+      <section
+        className="xl:padding-1
+            wide:padding-r padding-b main-section"
+      >
+        <div className="right-section">
+          <h1 className="text-4xl" style={{ color: "#F4CE14" }}>
+            little lemon
+          </h1>
+          <h3 style={{ color: "#fff" }}>Chicago</h3>
+          <p className="my-5 description">
+            we are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <a className="btn" href="/#">
+            Reserv a Table
+          </a>
+        </div>
+        <div className="left-section">
+          <img className="chef-photo" src={img} alt="chef holds food" />
+        </div>
+      </section>
+      <section>
+        <div className="flex justify-between mt-9">
+          <p className="text-5xl font-medium">This weeks specials</p>
+          <a href="/#" className="btn">
+            Online Menu
+          </a>
+        </div>
+        <img src={greeksalad} alt="greek salad" className="w-20 h-20" />
+        <p>12.99</p>
+        <p>lorem ipsum dolor sit amet consectetur</p>
+        <button>Order a delivery</button>
+        <img src={bruchetta} alt="bruchetta" />
+        <p>5.99</p>
+        <p>lorem ipsum dolor sit amet consectetur</p>
+        <button>Order a delivery</button>
+        <img src={lemondessert} alt="lemondessert" />
+        <p>5.00</p>
+        <p>lorem ipsum dolor sit amet consectetur</p>
+        <button>Order a delivery</button>
+      </section>
+    </main>
+  );
 };
 
 export default Main;
