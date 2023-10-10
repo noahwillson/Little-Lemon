cbc06840e7c10f2bdab2a2ad7dd94d40709d449b
Pegasus <nwillson2601@gmail.com>
2023-10-10 11:50:04 +0300

    nav ui

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,33 @@
+import React from "react";
+import Logo from "../images/Logo.svg";
+
 const Nav = () => {
-    return (
-        <nav class="flex-1 flex justify-content-flex-start align-items-center">
-            <ul>
-                <li>
-                    <a href="#home">Home</a>
-                </li>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#menu">Menu</a>
-                </li>
-                <li>
-                    <a href="#reservations">Reservations</a>
-                </li>
-                <li>
-                    <a href="#orderonline">Order Online</a>
-                </li>
-                <li>
-                    <a href="#login">Login</a>
-                </li>
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav className="flex py-6 justify-between items-center">
+      <img src={Logo} alt="Little Lemon" />
+
+      <ul className="list-none flex">
+        <li className=" mx-2">
+          <a href="#home">Home</a>
+        </li>
+        <li className="mx-2">
+          <a href="#about">About</a>
+        </li>
+        <li className="mx-2">
+          <a href="#menu">Menu</a>
+        </li>
+        <li className="mx-2">
+          <a href="#reservations">Reservations</a>
+        </li>
+        <li className="mx-2">
+          <a href="#orderonline">Order Online</a>
+        </li>
+        <li className="mx-2">
+          <a href="#login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
