af57b5ea8dcf5049d1ac7e22ef59413b902854fd
noahwillson <nwillson2601@gmail.com>
2023-10-13 15:30:00 +0300

    Removed page-container class

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import Logo from "../images/Logo.svg";
 
 const Nav = () => {
   return (
-    <nav className="page-container mx-auto flex py-6 justify-between items-center text-2xl font-medium">
+    <nav className="page-container bg-gray-300 mx-auto flex py-6 justify-between items-center text-2xl font-medium">
       <img src={Logo} alt="Little Lemon" className="w-72" />
 
       <ul className="list-none flex">
