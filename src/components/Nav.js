0d7f561bf687455348c7d4e0c8772832fa977a28
noahwillson <nwillson2601@gmail.com>
2023-10-14 15:30:00 +0300

    add container for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,11 @@ import Logo from "../images/Logo.svg";
 
 const Nav = () => {
   return (
-    <nav className="page-container bg-gray-300 mx-auto flex py-6 justify-between items-center text-2xl font-medium">
+    <nav className="container mx-auto flex py-6 justify-between items-center text-2xl font-medium">
       <img src={Logo} alt="Little Lemon" className="w-72" />
 
       <ul className="list-none flex">
-        <li className="active mx-4">
+        <li className="active:bg-yellow-500 mx-4">
           <Link to="/">Home</Link>
         </li>
         <li className="mx-4">
