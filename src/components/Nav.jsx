2ce9475a459b17c85923a8de96aabfed9b245007
noahwillson <nwillson2601@gmail.com>
2023-10-16 15:30:00 +0300

    created navitem

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import NavItem from "./NavItem";
 import Logo from "../images/Logo.svg";
+
+const navLinks = [
+  { id: 1, text: "Home", to: "/" },
+  { id: 2, text: "About", to: "/about" },
+  { id: 3, text: "Menu", to: "/menu" },
+  { id: 4, text: "Reservations", to: "/reservations" },
+  { id: 5, text: "Order Online", to: "/orderonline" },
+  { id: 6, text: "Login", to: "/login" },
+];
 
 const Nav = () => {
   return (
     <nav className="container mx-auto flex py-6 justify-between items-center text-2xl font-medium">
       <img src={Logo} alt="Little Lemon" className="w-72" />
 
-      <ul className="list-none flex">
-        <li className="active:bg-yellow-500 mx-4">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="mx-4">
-          <Link to="/about">About</Link>
-        </li>
-        <li className="mx-4">
-          <Link to="/menu">Menu</Link>
-        </li>
-        <li className="mx-4">
-          <Link to="/reservations">Reservations</Link>
-        </li>
-        <li className="mx-4">
-          <Link to="/orderonline">Order Online</Link>
-        </li>
-        <li className="ml-4">
-          <Link to="/login">Login</Link>
-        </li>
+      <ul className="flex gap-8">
+        {navLinks.map((link) => (
+          <NavItem key={link.id} path={link.to} text={link.text} />
+        ))}
       </ul>
     </nav>
   );
