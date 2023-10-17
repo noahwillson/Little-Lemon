2fc3283da20da5d728719cba15c88597e5ffd4a2
noahwillson <nwillson2601@gmail.com>
2023-10-17 15:30:00 +0300

    add navitem in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,16 @@
 import React from "react";
 import logo from "../images/Logo.svg";
+import { navLinks } from "./Nav";
+import NavItem from "./NavItem";
 const Footer = () => {
   return (
     <footer className="page-container mx-auto flex py-6 justify-between items-center text-center text-2xl font-medium">
       <img src={logo} alt="Little Lemon" className="w-72" />
       <ul>
         <p>Doormat Navigation</p>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#menu">Menu</a>
-        </li>
-        <li>
-          <a href="#reservations">Reservations</a>
-        </li>
-        <li>
-          <a href="#orderonline">Order Online</a>
-        </li>
-        <li>
-          <a href="#login">Login</a>
-        </li>
+        {navLinks.map((link) => (
+          <NavItem key={link.id} path={link.to} text={link.text} />
+        ))}
       </ul>
     </footer>
   );
